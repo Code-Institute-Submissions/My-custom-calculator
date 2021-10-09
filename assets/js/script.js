b4d8b7e964ab4e5476d8c4a78872dc2e4d087035
princessblessing <blessingnze3@gmail.com>
2021-10-09 20:47:58 +0000

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,19 +12,25 @@ document.addEventListener("DOMcontentLoaded", function() {
         let gameType = this.getAttribute("data-type");
         alert("You clicked ${gameType}")
     }
-    }
-
-)}
-        
-
+    })
     }
 })
+        
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user answer has been processed
+ */
 function runGame() {
 
-}
+    // Create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() *25) + 1;
+    let num2 = Math.floor(Math.random() *25) + 1;
+    }
+
 function checkAnswer() {
 
 }
+
 
 function calculatecorrectAnswer() {
 
