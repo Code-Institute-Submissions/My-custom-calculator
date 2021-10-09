5881e6dc84b9af8aa71403087514f3bf8b8fe0f6
princessblessing <blessingnze3@gmail.com>
2021-10-09 22:40:06 +0000

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,15 +34,30 @@ if (gameType === "Addition"){
     displayAdditionQuestion(num1, num2);
 }else{
     alert("Unknown game type: $(gameType");
-throw "Unknown game type: ${gameType}. Aborting!";
+throw "Unknown game type: ${gameType}. Aborting!"
+
 
 }
 function checkAnswer() {
 
 }
 
-
+/**
+ * Gets the operands (the numbers) and the operator (plus , minus etc)
+ * directly from the dom, and return the correct answer.
+ */
 function calculatecorrectAnswer() {
+
+let operand1 = parseInt(document.getElementById("operand1").innerText);
+let operand2 = parseInt(document.getElementById("operand2").innerText);
+let operator = document.getElementById("operator").innerText;
+
+if (operator === "+"){
+    return [operand1 + operand2, "Addition"];
+} else {
+    alert("unimplemented operator $[operator]");
+    throw "unimplemented operator $[operator], Aborting!";
+}
 
 }
 
