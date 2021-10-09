a7eb06ecacf7e84cccf6531c89b2c62bfcf76008
princessblessing <blessingnze3@gmail.com>
2021-10-09 18:42:53 +0000

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
 console.log("connected!")
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+
+function calculatecorrectAnswer() {
+
+}
+
+function incrementscore() {
+
+}
+
+function incrementwrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
